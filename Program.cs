e5cc5509aed414997c46ec12f428442a8445b56c
learningpath2025 <learningpath2025@outlook.com>
2025-10-28 16:11:45 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment10._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+            var filteredNumbers = numbers.Where(n => n > 80).ToList();
+            Console.WriteLine("The members of the list are:");
+            numbers.ForEach(n => Console.Write($"{n} "));
+            Console.WriteLine($"\n\nNumbers greater than 80:");
+            filteredNumbers.ForEach(n => Console.WriteLine($"{n}"));
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Assignment10.2.4
+
+- Write a program in C# Sharp to create a list of numbers and display numbers greater than 80.
+
+- Test Data :
+- The members of the list are :
+- 55 200 740 76 230 482 95
+
+- Expected Output :
+- The numbers greater than 80 are :
+- 200
+- 740
+- 230
+- 482
+- 95
